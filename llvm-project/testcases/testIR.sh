a463e20f34258b0ff493a6c5b8d5e2643e6c15b3
Owen Sebastian Hoffend <ohoffend@eecs583a.eecs.umich.edu>
2021-11-27 03:58:33 -0500

    Use opt from local build

--- llvm-project/testcases/testIR.sh
+++ llvm-project/testcases/testIR.sh
@@ -2,5 +2,5 @@
 rm *.bc
 rm *.ll
 clang -emit-llvm -S -O -Xclang -disable-llvm-passes test2.c
-opt -debug -slp-vectorizer -stats --debug-pass-manager -S test2.ll -o test2_opt.ll
+../build/bin/opt -debug -slp-vectorizer -stats --debug-pass-manager -S test2.ll -o test2_opt.ll
 diff test2_opt.ll test2.ll
