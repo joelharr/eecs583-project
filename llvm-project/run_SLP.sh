2040686299d76c02d7b5b036b5cae599155d6440
Rohan Girish Naik <rgnaik@eecs583b.eecs.umich.edu>
2021-12-14 17:45:24 -0500

    Added unrolled profiling data to SLP pass

--- llvm-project/run_SLP.sh
+++ llvm-project/run_SLP.sh
@@ -16,8 +16,14 @@ cd ..
 # This approach has an issue with -O2, so we are going to stick with default optimization level (-O0)
 clang -O0 -Xclang -disable-O0-optnone ./testcases/${1}.c -emit-llvm -c -o ${1}.bc
 
+# Apply loop unrolling
+# Add -time-passes to time the pass
+./viz.sh ${1}
+opt -mem2reg -simplifycfg -indvars -loop-unroll -unroll-count=4 -simplifycfg ${1}.bc -o ${1}_unrolled.bc
+./viz.sh ${1}_unrolled
+
 # Instrument profiler
-opt -pgo-instr-gen -instrprof ${1}.bc -o ${1}.prof.bc
+opt -pgo-instr-gen -instrprof ${1}_unrolled.bc -o ${1}.prof.bc
 # Generate binary executable with profiler embedded
 clang -fprofile-instr-generate ${1}.prof.bc -o ${1}.prof
 # Collect profiling data
@@ -25,11 +31,7 @@ clang -fprofile-instr-generate ${1}.prof.bc -o ${1}.prof
 # Translate raw profiling data into LLVM data format
 llvm-profdata merge -output=${1}_SLP.profdata default.profraw
 
-# Apply Superblock generation followed by SLP Vectorization
-# Add -time-passes to time the pass
-./viz.sh ${1}
-opt -mem2reg -simplifycfg -indvars -loop-unroll -unroll-count=4 -simplifycfg ${1}.bc -o ${1}_unrolled.bc
-./viz.sh ${1}_unrolled
+# run SLP pass on unrolled code
 opt -pgo-instr-use -pgo-test-profile-file=${1}_SLP.profdata -load ./build/${SLP_PATH}/ProfileSLP.so -ProfileSLP ${1}_unrolled.bc -o ${1}_SLP.bc
 ./viz.sh ${1}_SLP
 
